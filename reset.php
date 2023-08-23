11eb8a7dfb7354f52c09e01fd8cc3bd12cfda002
Im Zack <54569690+jakaprayudha@users.noreply.github.com>
2023-08-23 09:58:48 +0700

    Based

---
+++ reset.php
@@ -0,0 +1,86 @@
+<?php
+$page = "Reset";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta name="description" content="Sistem Informasi Manajemen Rumah Sakit | Medicine">
+   <meta name="keywords" content="SIMRS, Medicine, RME, Satu Sehat, BPJS">
+   <meta name="author" content="imzackdev">
+   <link rel="icon" href="assets/images/favicon.png" type="image/x-icon">
+   <link rel="shortcut icon" href="assets/images/favicon.png" type="image/x-icon">
+   <title><?= $page ?> | Medicine</title>
+   <!-- Google font-->
+   <link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i&amp;display=swap" rel="stylesheet">
+   <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900&amp;display=swap" rel="stylesheet">
+   <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
+   <!-- ico-font-->
+   <link rel="stylesheet" type="text/css" href="assets/css/vendors/icofont.css">
+   <!-- Themify icon-->
+   <link rel="stylesheet" type="text/css" href="assets/css/vendors/themify.css">
+   <!-- Flag icon-->
+   <link rel="stylesheet" type="text/css" href="assets/css/vendors/flag-icon.css">
+   <!-- Feather icon-->
+   <link rel="stylesheet" type="text/css" href="assets/css/vendors/feather-icon.css">
+   <!-- Plugins css start-->
+   <!-- Plugins css Ends-->
+   <!-- Bootstrap css-->
+   <link rel="stylesheet" type="text/css" href="assets/css/vendors/bootstrap.css">
+   <!-- App css-->
+   <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+   <link id="color" rel="stylesheet" href="assets/css/color-1.css" media="screen">
+   <!-- Responsive css-->
+   <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+</head>
+
+<body>
+   <!-- login page start-->
+   <div class="container-fluid">
+      <div class="row">
+         <div class="col-xl-7"><img class="bg-img-cover bg-center" src="assets/images/login/1.jpg" alt="looginpage"></div>
+         <div class="col-xl-5 p-0">
+            <div class="login-card">
+               <div>
+                  <div><a class="logo text-start" href="index.html"><img class="img-fluid for-light" src="assets/images/logo/login.png" alt="looginpage"><img class="img-fluid for-dark" src="assets/images/logo/logo_dark.png" alt="looginpage"></a></div>
+                  <div class="login-main">
+                     <form class="theme-form">
+                        <h4>Create Your Password</h4>
+                        <p>Enter your email & password to login</p>
+                        <div class="form-group">
+                           <label class="col-form-label">Email Address</label>
+                           <input class="form-control" type="email" required="" placeholder="Test@gmail.com">
+                        </div>
+                        <div class="form-group mb-0">
+                           <button class="btn btn-primary btn-block w-100" type="submit">Reset Password</button>
+                        </div>
+                        <p class="mt-4 mb-0 text-center">Don't have account ?<a class="ms-2" href="index">Sign In</a></p>
+                     </form>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- latest jquery-->
+      <script src="assets/js/jquery-3.5.1.min.js"></script>
+      <!-- Bootstrap js-->
+      <script src="assets/js/bootstrap/bootstrap.bundle.min.js"></script>
+      <!-- feather icon js-->
+      <script src="assets/js/icons/feather-icon/feather.min.js"></script>
+      <script src="assets/js/icons/feather-icon/feather-icon.js"></script>
+      <!-- scrollbar js-->
+      <!-- Sidebar jquery-->
+      <script src="assets/js/config.js"></script>
+      <!-- Plugins JS start-->
+      <!-- Plugins JS Ends-->
+      <!-- Theme js-->
+      <script src="assets/js/script.js"></script>
+      <!-- login js-->
+      <!-- Plugin used-->
+   </div>
+</body>
+
+</html>
